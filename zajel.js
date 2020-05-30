b51cd56011e6fd6cb08f0933fe311da6030e76d3
noha-isaed <thahernoha@gmail.com>
2020-05-30 18:14:17 +0300

    firstCommit

---
+++ zajel.js
@@ -0,0 +1,16 @@
+const express  = require("express");
+const app = express();
+const bodyparser  = require('body-parser');
+
+app.set('view engine' , 'ejs');
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"))
+
+app.get( "/" , function(req , res){
+ app.render("logIn");
+})
+
+app.listen (9999, function(){
+    console.log('Server started in port 9999');
+    });
+    
